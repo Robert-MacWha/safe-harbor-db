03133360c009615e41c87b36fcf7b0a0e49442b2
Robert MacWha <trebor.ahwcam@gmail.com>
2025-07-10 12:14:55 -0400

    feat: remove `refreshChildContracts` functionality

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"SHDB/pkg/client"
 	"SHDB/pkg/config"
 	"SHDB/pkg/contracts/safeharbor"
 	"SHDB/pkg/defiliama"
 	"SHDB/pkg/firebase"
-	"SHDB/pkg/scan"
 	"context"
 	"fmt"
 	"log/slog"
 	"math/big"
 	"os"
-	"time"
 
 	"cloud.google.com/go/firestore"
 	"github.com/ethereum/go-ethereum/common"
@@ -20,7 +17,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 	"github.com/lmittmann/tint"
-	"github.com/nanmu42/etherscan-api"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -190,6 +186,7 @@ func runRefreshTvl(cCtx *cli.Context) error {
 
 	for _, doc := range documents {
 		slug := doc.Ref.ID
+		slog.Info("Refreshing TVL for protocol", "slug", slug)
 		err = refreshTvl(fClient, protocolCol, slug)
 		if err != nil {
 			slog.Warn("refreshTvl", "error", err)
@@ -392,107 +389,8 @@ func refreshChildContracts(
 	slug string,
 	chainCfg map[int]config.ChainCfg,
 ) error {
-	protocolDocRef := fClient.Collection(protocolCol).Doc(slug)
-	protocolDoc, err := protocolDocRef.Get(context.Background())
-	if err != nil {
-		return fmt.Errorf("firestore.Get: %w", err)
-	}
-
-	var protocol firebase.Protocol
-	err = protocolDoc.DataTo(&protocol)
-	if err != nil {
-		return fmt.Errorf("firestore.DataTo: %w", err)
-	}
-
-	agreementId := protocol.SafeHarborAgreement.ID
-	agreementDocRef := fClient.Collection(agreementCol).Doc(agreementId)
-	agreementDoc, err := agreementDocRef.Get(context.Background())
-	if err != nil {
-		return fmt.Errorf("firestore.Get: %w", err)
-	}
-
-	var agreement firebase.SafeHarborAgreement
-	err = agreementDoc.DataTo(&agreement)
-	if err != nil {
-		return fmt.Errorf("firestore.DataTo: %w", err)
-	}
-
-	totalChildren := 0
-	for i, chain := range agreement.Chains {
-		if _, exists := chainCfg[chain.ID]; !exists {
-			slog.Info("Chain not found in chain config, skipping", "chain", chain.ID)
-			continue
-		}
-
-		eClient, err := client.Dial(chainCfg[chain.ID].RpcUrl)
-		if err != nil {
-			return fmt.Errorf("rpc.Dial: %w", err)
-		}
-
-		sClient := scan.NewRateLimitedClient(etherscan.NewCustomized(etherscan.Customization{
-			Timeout: 20 * time.Second,
-			Key:     chainCfg[chain.ID].ScanKey,
-			BaseURL: chainCfg[chain.ID].ScanUrl,
-			Verbose: false,
-		}))
-
-		block, err := eClient.BlockNumber(context.Background())
-		if err != nil {
-			return fmt.Errorf("rpc.BlockNumber: %w", err)
-		}
-
-		lastIndexed := chain.LastIndexedBlock
-		for j, account := range chain.Accounts {
-			log := slog.Default().With("account", account.Address)
-			if account.ChildContractScope == firebase.ChildContractScopeNone {
-				log.Debug("Skipping account", "reason", "ChildContractScopeNone")
-				continue
-			}
-
-			var endBlock int = int(block)
-			if account.ChildContractScope == firebase.ChildContractScopeExistingOnly {
-				endBlock = agreement.CreatedBlock
-			}
-
-			log.Debug("Refreshing child contracts", "scope", account.ChildContractScope, "from", lastIndexed, "to", endBlock)
-			var accountAddr = common.HexToAddress(account.Address)
-			children, err := getAccountChildren(eClient, sClient, accountAddr, lastIndexed, &endBlock)
-			if err != nil {
-				return fmt.Errorf(
-					"getAccountChildren(accountAddr=%v, chain=%v, agreement=%v): %w",
-					accountAddr,
-					chain.ID,
-					agreement.AgreementAddress,
-					err,
-				)
-			}
-
-			if agreement.Chains[i].Accounts[j].Name == "" {
-				agreement.Chains[i].Accounts[j].Name = sClient.ContractName(account.Address)
-			}
-			agreement.Chains[i].Accounts[j].Children = children
-			totalChildren += len(children)
-		}
-
-		agreement.Chains[i].LastIndexedBlock = int(block)
-		slog.Info(
-			"Successfully refreshed chain",
-			"agreement", agreement.AgreementAddress,
-			"chain", chain.ID,
-			"start", lastIndexed,
-			"end", block,
-		)
-	}
-
-	// Upload updated agreement to firestore
-	_, err = agreementDocRef.Set(context.Background(), agreement)
-	if err != nil {
-		return fmt.Errorf("firestore.Set: %w", err)
-	}
-
-	slog.Info("Successfully refreshed child contracts", "protocol", slug, "agreement", agreement.AgreementAddress, "total", totalChildren)
-
-	return nil
+	_, _, _, _, _ = fClient, protocolCol, agreementCol, slug, chainCfg
+	return fmt.Errorf("refreshChildContracts not implemented - issue #16")
 }
 
 func getCollectionNames(cCtx *cli.Context) (protocolCol string, agreementCol string) {
@@ -506,200 +404,4 @@ func getCollectionNames(cCtx *cli.Context) (protocolCol string, agreementCol str
 	}
 
 	return protocolCol, agreementCol
-}
-
-func getAccountChildren(
-	eClient client.EthClient,
-	sClient scan.Client,
-	accountAddr common.Address,
-	startBlock int,
-	endBlock *int,
-) (children []firebase.ChildAccount, err error) {
-	// Get all txhashes that interacted with this account
-	txns, err := getAccountTxns(sClient, accountAddr, startBlock, endBlock)
-	if err != nil {
-		return nil, fmt.Errorf("getAccountTxns: %w", err)
-	}
-
-	if len(txns) == 0 {
-		return []firebase.ChildAccount{}, nil
-	}
-	slog.Debug("Found transactions", "account", accountAddr, "count", len(txns))
-
-	// Trace all transactions to find subcontracts
-	// TODO: This may encounter issues if some function signatures conditionally create subcontracts
-	noSubContractFunctionSignatures := make(map[string]bool)
-	children = []firebase.ChildAccount{}
-	for _, txn := range txns {
-		txInput := txn.Input
-		if len(txn.Input) > 10 {
-			txInput = txn.Input[:10]
-		}
-
-		if _, exists := noSubContractFunctionSignatures[txInput]; exists {
-			continue
-		}
-
-		newChildren := getTxnChildren(eClient, sClient, common.HexToHash(txn.Hash), accountAddr, startBlock, endBlock)
-		if len(newChildren) == 0 {
-			noSubContractFunctionSignatures[txInput] = true
-		}
-
-		children = append(children, newChildren...)
-	}
-
-	if len(children) != 0 {
-		slog.Debug("Found child contracts", "account", accountAddr, "count", len(children))
-	}
-
-	return children, nil
-}
-
-// getAccountTxns returns all transactions that interacted with an account, including internal transactions
-//
-// ? May return duplicate transactions, but they will be infrequent. Processing duplicates is probably more performant than
-// ? trying to filter them out for large sets.
-func getAccountTxns(
-	sClient scan.Client,
-	accountAddr common.Address,
-	startBlock int,
-	endBlock *int,
-) ([]scan.Tx, error) {
-	var txns []scan.Tx
-	hashmap := map[string]bool{}
-
-	// TODO: Merge these two loops if you can find a clean approach
-	// TODO: Write smarter rate limit handling
-	// Fetch regular transactions
-	lastRegularIndexed := startBlock
-	errCount := 0
-	for {
-		normalTxns, err := sClient.NormalTxByAddress(accountAddr.String(), &lastRegularIndexed, endBlock, 0, 0, true)
-		if err != nil && err.Error() == EtherscanErrNoTxns {
-			break
-		}
-
-		if err != nil {
-			slog.Warn(
-				"etherscan.NormalTxByAddress",
-				"addr", accountAddr,
-				"start", lastRegularIndexed,
-				"end", endBlock,
-				"error", err,
-			)
-			errCount += 1
-			time.Sleep(5 * time.Second)
-			if errCount > 5 {
-				return nil, fmt.Errorf("etherscan.NormalTxByAddress: %w", err)
-			}
-
-			continue
-		}
-		errCount = 0
-
-		updatedLastIndexed := false
-		for _, txn := range normalTxns {
-			if _, exists := hashmap[txn.Hash]; exists {
-				continue
-			}
-
-			hashmap[txn.Hash] = true
-
-			txns = append(txns, txn)
-			if txn.BlockNumber > lastRegularIndexed {
-				lastRegularIndexed = txn.BlockNumber
-				updatedLastIndexed = true
-			}
-		}
-
-		// Break if no new transactions are found
-		if !updatedLastIndexed {
-			break
-		}
-
-	}
-
-	// Fetch internal transactions
-	lastInternalIndexed := startBlock
-	for {
-		internalTxns, err := sClient.InternalTxByAddress(accountAddr.String(), &lastInternalIndexed, endBlock, 0, 0, true)
-		if err != nil && err.Error() == EtherscanErrNoTxns {
-			break
-		}
-
-		if err != nil {
-			slog.Warn(
-				"etherscan.InternalTxByAddress",
-				"addr", accountAddr,
-				"start", lastRegularIndexed,
-				"end", endBlock,
-				"error", err,
-			)
-			errCount += 1
-			time.Sleep(5 * time.Second)
-			if errCount > 5 {
-				return nil, fmt.Errorf("etherscan.InternalTxByAddress: %w", err)
-			}
-
-			continue
-		}
-		errCount = 0
-
-		updatedLastIndexed := false
-		for _, txn := range internalTxns {
-			if _, exists := hashmap[txn.Hash]; exists {
-				continue
-			}
-
-			hashmap[txn.Hash] = true
-
-			txns = append(txns, txn)
-			if txn.BlockNumber > lastInternalIndexed {
-				lastInternalIndexed = txn.BlockNumber
-				updatedLastIndexed = true
-			}
-		}
-
-		// Break if no new transactions are found
-		if !updatedLastIndexed {
-			break
-		}
-
-	}
-
-	return txns, nil
-}
-
-func getTxnChildren(
-	eClient client.EthClient,
-	sClient scan.Client,
-	hash common.Hash,
-	accountAddr common.Address,
-	startBlock int,
-	endBlock *int,
-) []firebase.ChildAccount {
-	calls, err := eClient.DebugTraceTransaction(hash)
-	if err != nil {
-		slog.Warn("Failed to debug trace transaction", "account", accountAddr, "hash", hash, "error", err)
-		return nil
-	}
-
-	newChildren := []firebase.ChildAccount{}
-	for _, call := range calls.Flatten() {
-		if call.Type == "CREATE" && call.From == accountAddr && call.To != nil {
-			recursiveChildren, err := getAccountChildren(eClient, sClient, *call.To, startBlock, endBlock)
-			if err != nil {
-				slog.Warn("Failed to get recursive children", "account", accountAddr, "error", err)
-				return nil
-			}
-
-			newChildren = append(newChildren, firebase.ChildAccount{
-				Address: call.To.String(),
-				Name:    sClient.ContractName(call.To.String()),
-			})
-			newChildren = append(newChildren, recursiveChildren...)
-		}
-	}
-
-	return newChildren
 }
